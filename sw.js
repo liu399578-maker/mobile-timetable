1f051cbdb403d6f67bcad242fcaee51844ca2a9e
liu399578-maker <liu399578@gmail.com>
2026-08-15 10:50:40 -0600

    改版蓝色单屏课表 (#1)
    Co-authored-by: liu399578-maker <liu399578-maker@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'timetable-2026-v2';
+const CACHE = 'timetable-2026-v3';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './timetable.ics'];
 
 self.addEventListener('install', event => {
@@ -13,9 +13,17 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+      return response;
+    }).catch(() => caches.match('./index.html')));
+    return;
+  }
   event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
     return response;
-  }).catch(() => caches.match('./index.html'))));
+  })));
 });
