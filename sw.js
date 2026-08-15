edeccebac55357dc6f5bdc9f1e575357590338f1
liu399578-maker <liu399578@gmail.com>
2026-08-15 11:27:28 -0600

    精简周视图卡片布局 (#3)
    Co-authored-by: liu399578-maker <liu399578-maker@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'timetable-2026-v4';
+const CACHE = 'timetable-2026-v5';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './timetable.ics'];
 
 self.addEventListener('install', event => {
